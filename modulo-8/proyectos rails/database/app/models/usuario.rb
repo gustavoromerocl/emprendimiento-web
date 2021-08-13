2477d300cab5808078d5ac81732464507168e0c2
romerodevelopments <gandresrp@gmail.com>
2021-08-13 12:07:20 -0400

    autenticacion de usuarios con la gema bcrypt

--- modulo-8/proyectos rails/database/app/models/usuario.rb
+++ modulo-8/proyectos rails/database/app/models/usuario.rb
@@ -1,4 +1,5 @@
 class Usuario < ApplicationRecord
+    has_secure_password
     VALIDA_FORMATO_CORREO = /([a-zA-Z0-9]([^ @&%$\\\/()=?¿!.,:;]?|\d?)+[a-zA-Z0-9][\.]){1,2}/
     validates   :nombre_usuario, :direccion, :escolaridad, :email, :profesion, presence: true
     validates   :nombre_usuario,  uniqueness: true
